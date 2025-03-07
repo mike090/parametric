1c0311c369c80b7e9434e8f53af2240890ed0822
mike09 <mike09@mail.ru>
2025-03-07 07:42:43 +0300

    Small fixes

--- src/parametric/transformation_factory.rb
+++ src/parametric/transformation_factory.rb
@@ -13,4 +13,22 @@ module Parametric
 			Geom::Transformation.new environ.origin, environ.x_direction, environ.y_direction
 		end
 	end
+
+	class TransformationFactoryBuilder < Parametric::FactoryBuilder
+		factory_class TransformationFactory
+
+		class << self
+			private
+
+			def x_axis_directed_along(value = nil, &block)
+				@factory.params.set :x_direction, value, &block
+			end
+
+			def y_axis_directed_along(value = nil, &block)
+				@factory.params.set :y_direction, value, &block
+			end
+		end
+	end
+
+	Parametric.register_builder :position, TransformationFactoryBuilder
 end
