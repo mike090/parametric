162894d9ad4ed8b3739c7d2652d5a98c3c0ffdee
mike09 <mike09@mail.ru>
2025-03-11 00:58:16 +0300

    Improve drawing factory test

--- tests/factories/tc_drawing_factory.rb
+++ tests/factories/tc_drawing_factory.rb
@@ -2,6 +2,12 @@ require 'testup/testcase'
 require_relative '../../src/parametric/factories/drawing_factory'
 
 class TC_DrawingFactory < TestUp::TestCase
+	attr_accessor :temp_geometry
+
+	def teardown
+    temp_geometry.erase! if temp_geometry
+  end
+
 	def test_transformation_applies
 		factory_class = Class.new Parametric::DrawingFactory
 		factory_class.define_method :draw do |_env|
@@ -9,7 +15,7 @@ class TC_DrawingFactory < TestUp::TestCase
 			group.entities.add_face([0,0,0], [1,0,0], [1,1,0], [0,1,0]).pushpull(-1)
 			group
 		end
-		group = factory_class.new.build position: Geom::Transformation.new([1,0,0])
-		assert_equal [1,0,0], group.transformation.origin.to_a
+		self.temp_geometry = factory_class.new.build position: Geom::Transformation.new([1,0,0])
+		assert_equal [1,0,0], self.temp_geometry.transformation.origin.to_a
 	end
 end
