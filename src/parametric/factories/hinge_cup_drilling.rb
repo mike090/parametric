43bb20094c0162892cfea1f9e9cfe8d8fdcea57d
mike09 <mike09@mail.ru>
2025-04-06 09:49:14 +0300

    refactoring: move hinge cup mapper to HingeCupDrilling namespace

--- src/parametric/factories/hinge_cup_drilling.rb
+++ src/parametric/factories/hinge_cup_drilling.rb
@@ -1,5 +1,6 @@
 require_relative 'drawing_factory'
 require_relative 'hinge_cup_mapper'
+require_relative 'drilling'
 
 module Parametric
 	class HingeCupDrilling < DrawingFactory
@@ -19,13 +20,12 @@ module Parametric
 		end
 
 		def draw
-			drilling_map = Parametric::HingeCupMapper.new.build map: hinge_map, bore: bore_distance
+			drilling_map = Mapper.new.build map: hinge_map, bore: bore_distance
 
 			container.add_group.tap do |cup_drilling|
-				drill = Parametric::Drilling.new container: cup_drilling.entities
-				drill.build **cup_drilling_params, position: drilling_map.cup_center
+				drill.build **cup_drilling_params, position: drilling_map.cup_center, container: cup_drilling.entities
 				drilling_map.cup_fixes.each do |pos|
-					drill.build **fixing_drilling_params, position: pos
+					drill.build **fixing_drilling_params, position: pos, container: cup_drilling.entities
 				end
 				cup_drilling.name = 'Hinge cup drilling'
 				cup_drilling.set_attribute 'Parametric', 'type', 'drilling_group'
@@ -37,6 +37,45 @@ module Parametric
 		def parse_params(**params)
 			params[:bore_distance] = params.delete :bore if params[:bore]
 			super
+		end
+
+		def drill
+			@drill ||= Parametric::Drilling.new
+		end
+
+		class Mapper < Factory
+			required_params :bore_distance, :interfixes_distance, :cup_fixes_offset
+			define_param_readers :bore_distance, :interfixes_distance, :cup_fixes_offset
+
+			def do_build
+				hinge_cup_center = Geom::Point3d.new(0, bore_distance + (35.0 / 2).mm)
+				Parametric::Maps::HingeCup.new(
+					hinge_cup_center,
+					hinge_cup_center.offset([(-interfixes_distance / 2),cup_fixes_offset]),
+					hinge_cup_center.offset([(interfixes_distance / 2),cup_fixes_offset])
+				)
+			end
+
+			private
+
+			def parse_params(**params)
+				params[:bore_distance] = params.delete :bore if params[:bore]
+				hinge_map = params[:map]
+				params.merge!try_parse_map(hinge_map) if hinge_map
+				super
+			end
+
+			def try_parse_map(map)
+				raise "invalid hinge cup map. Expected string but #{map} has been received" unless String === map
+
+				values = (map.scan /^(\d+)[x|*|х]([0-9]*[.,]?[0-9]+)/).first
+				if values && values.length == 2
+					values.map! { |value| value.sub(',', '.').to_f.mm }
+					[:interfixes_distance, :cup_fixes_offset].zip(values).to_h
+				else
+					raise "invalid hinge cup map #{map}"
+				end
+			end
 		end
 	end
 end
--- tests/factories/tc_hinge_cup_mapper.rb
+++ tests/factories/tc_hinge_cup_mapper.rb
@@ -4,7 +4,7 @@ require_relative '../../src/parametric/models/maps/hinge_cup'
 
 class TC_HingeCupMapper < TestUp::TestCase
 	def test_created_map
-		cup_map = Parametric::HingeCupMapper.new.build(bore: 4.mm, map: '52x5.5')
+		cup_map = Parametric::HingeCupDrilling::Mapper.new.build(bore: 4.mm, map: '52x5.5')
 		assert_instance_of Parametric::Maps::HingeCup, cup_map
 		assert_equal [0, 21.5.mm, 0], cup_map.cup_center.to_a
 		assert cup_map.cup_fixes.map(&:to_a).include?([-26.mm, 27.mm, 0.mm])
@@ -12,13 +12,13 @@ class TC_HingeCupMapper < TestUp::TestCase
 	end
 
 	def test_invalid_map_raises
-		e = assert_raises { Parametric::HingeCupMapper.new(bore: 4, map: 'fake') }
+		e = assert_raises { Parametric::HingeCupDrilling::Mapper.new(bore: 4, map: 'fake') }
 		assert_match 'fake', e.message
 	end
 
 	def test_with_regular_params
 		assert_instance_of Parametric::Maps::HingeCup,
-			Parametric::HingeCupMapper.new.build(
+			Parametric::HingeCupDrilling::Mapper.new.build(
 				bore: 4.mm,
 				interfixes_distance: 45.mm,
 				cup_fixes_offset: 9.5
